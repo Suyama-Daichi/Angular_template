ffa57210739b74ef02dd7d7daad4f814efb0cfda
SuyamaDaichi <symdit@gmail.com>
2020-07-14 21:13:55 +0900

    add
    ログインコンポーネント作成&ルーティング設定

--- SampleWebApp/src/app/app-routing.module.ts
+++ SampleWebApp/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  {path: }
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
