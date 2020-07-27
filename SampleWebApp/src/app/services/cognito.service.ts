edea4b8c5879b2b68d208f67005bcf46c4f3ae5d
SuyamaDaichi <symdit@gmail.com>
2020-07-27 23:25:30 +0900

    update
    CognitoServiceを更新

--- SampleWebApp/src/app/services/cognito.service.ts
+++ SampleWebApp/src/app/services/cognito.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import {
   CognitoUserPool,
   CognitoUser,
-  AuthenticationDetails
+  AuthenticationDetails,
+  CognitoUserAttribute
 } from 'amazon-cognito-identity-js';
 import * as AWS from 'aws-sdk';
 import { environment } from 'src/environments/environment';
@@ -22,6 +23,19 @@ export class CognitoService {
     AWS.config.region = environment.aws.region;
     this.poolData = { UserPoolId: environment.aws.userPoolId, ClientId: environment.aws.clientId };
     this.userPool = new CognitoUserPool(this.poolData);
+  }
+
+  // サインアップ処理
+  signup(userid: string, password: string, attributes: CognitoUserAttribute[]): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.userPool.signUp(userid, password, attributes, null, (err, success) => {
+        if (success) {
+          resolve(success);
+        } else if (err) {
+          reject(err);
+        }
+      });
+    });
   }
 
   // ログイン処理
@@ -64,10 +78,9 @@ export class CognitoService {
     const cognitoUser = this.userPool.getCurrentUser();
     return new Promise((resolve, reject) => {
       if (cognitoUser === null) {
-        // resolve(false);
         reject(cognitoUser);
       }
-      cognitoUser.getSession((err, session) => {
+      cognitoUser.getSession((err: any, session: { isValid: () => any; }) => {
         if (err) {
           reject(err);
         } else {
@@ -119,15 +132,33 @@ export class CognitoService {
     };
     const cognitoUser = this.userPool.getCurrentUser();
     return new Promise((resolve, reject) => {
-      cognitoUser.getSession((err1, result2) => {
-        if (result2) {
-          cognitoUser.updateAttributes([emailAttr], (err2, result3) => {
-            if (result3) {
-              resolve(result3);
+      cognitoUser.getSession((err1: any, result1: any) => {
+        if (result1) {
+          cognitoUser.updateAttributes([emailAttr], (err2, result2) => {
+            if (result2) {
+              resolve(result2);
             } else {
               reject(err2);
             }
           });
+        }
+      });
+    });
+  }
+
+  // アカウントをconfirm状態にする
+  async confirmAccont(userName: string, confirmationCode: string) {
+    return new Promise((resolve, reject) => {
+      const userData = {
+        Username: userName,
+        Pool: this.userPool,
+      };
+      const cognitoUser = new CognitoUser(userData);
+      cognitoUser.confirmRegistration(confirmationCode, false, (err, success) => {
+        if (success) {
+          resolve(success);
+        } else {
+          reject(err);
         }
       });
     });
