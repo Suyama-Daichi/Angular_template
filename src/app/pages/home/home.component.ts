eb76300b6a34ac4986f30747a7a30640a1bff750
SuyamaDaichi <symdit@gmail.com>
2020-09-03 12:58:11 +0900

    フォームオブジェクトを作ってフォームを作る

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,3 +1,4 @@
+import { Field } from './../../components/parts/form-field/form-field.component';
 import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { Option } from 'src/app/components/parts/form-field/form-field.component';
@@ -9,19 +10,25 @@ import { Option } from 'src/app/components/parts/form-field/form-field.component
 })
 export class HomeComponent implements OnInit {
   options: Option[] = [
-    {value: 'OptionA', label: 'オプションA'},
-    {value: 'OptionB', label: 'オプションB'}
+    { value: 'OptionA', label: 'オプションA' },
+    { value: 'OptionB', label: 'オプションB' }
   ];
+  fields: Field[] = [
+    { id: 'fieldA', name: '入力A', fieldType: 'input', inputType: 'text', required: true },
+    { id: 'fieldB', name: '入力B', fieldType: 'input', inputType: 'number' }
+  ]
   formGroup: FormGroup = this.fb.group({});
   constructor(
     private fb: FormBuilder
   ) { }
 
   ngOnInit(): void {
+    console.log(this.formGroup);
   }
 
-  catchFormData(name: string, e: FormControl) {
+  formInitialized(name: string, e: FormControl) {
     this.formGroup.setControl(name, e);
+    console.log(this.formGroup);
   }
 
 }
