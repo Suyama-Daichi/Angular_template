777cb02533fc26a949ef31eb5e50adaaf8338d92
SuyamaDaichi <symdit@gmail.com>
2020-09-30 23:38:43 +0900

    FirebaseModule：作成、分割

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,9 @@
+import { FirebaseModuleModule } from './modules/firebase-module/firebase-module.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { environment } from 'src/environments/environment';
 import { LoginComponent } from './pages/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
 
@@ -19,9 +16,7 @@ import { HeaderComponent } from './components/header/header.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAnalyticsModule,
-    AngularFirestoreModule
+    FirebaseModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
